d604c2ce785e96e16ece8fa6937c5c45d0520b1d
Administrator <Administrator@PC-4-31>
2013-10-17 00:06:56 +0900

    프로필힘든세동

--- Collage_GoddessOfSkin-prototype/gen/com/collage/goddessofskin/prototype/R.java
+++ Collage_GoddessOfSkin-prototype/gen/com/collage/goddessofskin/prototype/R.java
@@ -19,15 +19,22 @@ public final class R {
         public static final int selector_drawer_list_item=0x7f080000;
     }
     public static final class dimen {
-        public static final int drawer_menus_marginTop=0x7f060004;
-        public static final int drawer_width=0x7f060003;
-        public static final int profile_avatar_height=0x7f060008;
-        public static final int profile_avatar_margin=0x7f06000a;
-        public static final int profile_avatar_padding=0x7f060009;
-        public static final int profile_avatar_width=0x7f060007;
-        public static final int profile_height=0x7f060006;
-        public static final int profile_name_margin=0x7f06000b;
-        public static final int profile_width=0x7f060005;
+        public static final int drawer_menus_marginTop=0x7f060005;
+        public static final int drawer_width=0x7f060004;
+        public static final int drawer_width_1=0x7f060003;
+        public static final int profile_avatar_height=0x7f06000a;
+        public static final int profile_avatar_height_1=0x7f06000c;
+        public static final int profile_avatar_height_2=0x7f06000d;
+        public static final int profile_avatar_margin=0x7f060011;
+        public static final int profile_avatar_margin_Top=0x7f060010;
+        public static final int profile_avatar_margin_right=0x7f06000f;
+        public static final int profile_avatar_padding=0x7f06000e;
+        public static final int profile_avatar_width=0x7f060009;
+        public static final int profile_avatar_width_1=0x7f06000b;
+        public static final int profile_height=0x7f060007;
+        public static final int profile_height_1=0x7f060008;
+        public static final int profile_name_margin=0x7f060012;
+        public static final int profile_width=0x7f060006;
         public static final int splash_progress_margin=0x7f060000;
         public static final int type_button_padding=0x7f060002;
         public static final int type_buttonbar_margin=0x7f060001;
@@ -47,9 +54,9 @@ public final class R {
         public static final int weather_3=0x7f02000b;
     }
     public static final class id {
-        public static final int GalleryText=0x7f0a0013;
+        public static final int GalleryText=0x7f0a0016;
         public static final int ListView01=0x7f0a000d;
-        public static final int Schedule_butten=0x7f0a0011;
+        public static final int Schedule_butten=0x7f0a0014;
         public static final int act_main_content_frame=0x7f0a0001;
         public static final int act_main_drawer_layout=0x7f0a0000;
         public static final int act_main_left_drawer=0x7f0a0002;
@@ -62,28 +69,30 @@ public final class R {
         public static final int act_type_choice_btn_dry=0x7f0a000a;
         public static final int act_type_choice_btn_oily=0x7f0a0009;
         public static final int act_type_choice_title=0x7f0a0008;
-        public static final int action_websearch=0x7f0a0024;
-        public static final int btn_015=0x7f0a000e;
+        public static final int action_websearch=0x7f0a0026;
+        public static final int btn_015=0x7f0a0011;
         public static final int btn_graph=0x7f0a000c;
-        public static final int drawer_menus=0x7f0a0020;
-        public static final int drawer_profile_avatar=0x7f0a0021;
-        public static final int drawer_profile_name=0x7f0a0022;
-        public static final int drawer_profile_skin=0x7f0a001f;
-        public static final int drawer_profile_type=0x7f0a0023;
-        public static final int gallery=0x7f0a000f;
-        public static final int image=0x7f0a0012;
-        public static final int imageView1=0x7f0a001a;
-        public static final int imageView2=0x7f0a001d;
-        public static final int today_weather_item=0x7f0a0010;
-        public static final int tv_humidity=0x7f0a0018;
-        public static final int tv_name_Of_Space=0x7f0a0015;
-        public static final int tv_temperature=0x7f0a0016;
-        public static final int tv_weather_Type_Name=0x7f0a0014;
-        public static final int tv_wind=0x7f0a0017;
-        public static final int ult_15=0x7f0a001b;
-        public static final int ult_20=0x7f0a001c;
-        public static final int ult_30=0x7f0a001e;
-        public static final int ult_text=0x7f0a0019;
+        public static final int drawer_menus=0x7f0a0022;
+        public static final int drawer_profile_avatar=0x7f0a0023;
+        public static final int drawer_profile_name=0x7f0a0024;
+        public static final int drawer_profile_skin=0x7f0a000e;
+        public static final int drawer_profile_type=0x7f0a0025;
+        public static final int gallery=0x7f0a0012;
+        public static final int image=0x7f0a0015;
+        public static final int imageView1=0x7f0a001d;
+        public static final int imageView2=0x7f0a0020;
+        public static final int skin_button=0x7f0a0010;
+        public static final int skin_image=0x7f0a000f;
+        public static final int today_weather_item=0x7f0a0013;
+        public static final int tv_humidity=0x7f0a001b;
+        public static final int tv_name_Of_Space=0x7f0a0018;
+        public static final int tv_temperature=0x7f0a0019;
+        public static final int tv_weather_Type_Name=0x7f0a0017;
+        public static final int tv_wind=0x7f0a001a;
+        public static final int ult_15=0x7f0a001e;
+        public static final int ult_20=0x7f0a001f;
+        public static final int ult_30=0x7f0a0021;
+        public static final int ult_text=0x7f0a001c;
     }
     public static final class layout {
         public static final int act_main=0x7f030000;

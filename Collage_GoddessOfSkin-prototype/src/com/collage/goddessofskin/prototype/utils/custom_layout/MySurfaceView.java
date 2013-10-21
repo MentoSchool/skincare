3f2f4d4a47e41af670a953d79ecd87fcf08d2417
user <user@192.168.0.6>
2013-10-22 04:35:06 +0900

    commit

--- Collage_GoddessOfSkin-prototype/src/com/collage/goddessofskin/prototype/utils/custom_layout/MySurfaceView.java
+++ Collage_GoddessOfSkin-prototype/src/com/collage/goddessofskin/prototype/utils/custom_layout/MySurfaceView.java
@@ -1,9 +1,7 @@
 package com.collage.goddessofskin.prototype.utils.custom_layout;
 
-
-
-
-
+import android.R;
+import android.R.color;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,31 +11,29 @@ import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+public class MySurfaceView extends SurfaceView implements
+		SurfaceHolder.Callback {
 
 	Paint Pnt = new Paint();
-	
+
 	MyViewThread mThread;
-	
-	
-	
+
 	public MySurfaceView(Context context) {
 		super(context);
 		getHolder().addCallback(this);
 	}
-	
-	public MySurfaceView(Context context,AttributeSet as) {
+
+	public MySurfaceView(Context context, AttributeSet as) {
 		// TODO Auto-generated constructor stub
-		super(context,as);
+		super(context, as);
 		getHolder().addCallback(this);
 	}
-	
-	public MySurfaceView(Context context,AttributeSet as,int style) {
+
+	public MySurfaceView(Context context, AttributeSet as, int style) {
 		// TODO Auto-generated constructor stub
-		super(context,as,style);
+		super(context, as, style);
 		getHolder().addCallback(this);
 	}
-	
 
 	public void surfaceCreated(SurfaceHolder holder) {
 		mThread = new MyViewThread(getHolder()); // GameThread 생성
@@ -66,9 +62,7 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 class MyViewThread extends Thread {
 	private SurfaceHolder surfaceholder;
 	private boolean running = true;
-
-	
-	
+	Paint Pnt = new Paint();
 	int imgX = 0;
 	int imgY = 0;
 
@@ -79,7 +73,7 @@ class MyViewThread extends Thread {
 	public void run() {
 		Canvas c;
 
-		while (imgX<100) {
+		while (imgX < 120) {
 			c = null;
 			try {
 				c = surfaceholder.lockCanvas(null);
@@ -87,7 +81,9 @@ class MyViewThread extends Thread {
 					// drawBackground(c);
 					// drawMyBitmap(c);
 					drawCricle(c);
-					
+					drawCricle_1(c);
+					drawCricle_2(c);
+
 					imgX++;
 
 				}
@@ -100,46 +96,69 @@ class MyViewThread extends Thread {
 		}
 	}
 
+	private void drawCricle_2(Canvas canvas) {
+		// TODO Auto-generated method stub
+
+		RectF rf3 = new RectF();
+		Pnt.setColor(Color.RED);
+		rf3.set(50, 50, 500, 500);
+		canvas.drawArc(rf3, 240, imgX, true, Pnt);
+		canvas.drawText("Arc", 200, 470, Pnt);
+
+		RectF rf4 = new RectF();
+		Pnt.setColor(Color.WHITE);
+		rf4.set(150, 150, 400, 400);
+		canvas.drawArc(rf4, 240, imgX, true, Pnt);
+
+	}
+
+	private void drawCricle_1(Canvas canvas) {
+		// TODO Auto-generated method stub
+
+		RectF rf3 = new RectF();
+		Pnt.setColor(Color.BLUE);
+		rf3.set(50, 50, 500, 500);
+		canvas.drawArc(rf3, 120, imgX, true, Pnt);
+		canvas.drawText("Arc", 200, 470, Pnt);
+
+		RectF rf4 = new RectF();
+		Pnt.setColor(Color.WHITE);
+		rf4.set(150, 150, 400, 400);
+		canvas.drawArc(rf4, 120, imgX, true, Pnt);
+
+	}
+
 	private void drawCricle(Canvas canvas) {
 		// TODO Auto-generated method stub
 
-		Paint Pnt = new Paint();
-		
-		 Pnt.setColor(Color.WHITE);
-		 canvas.drawPaint(Pnt);
-		
-		
-		
-		
-		RectF r = new RectF(9, 100, 500, 100);
-		RectF r2 = new RectF(190, 350, 290, 450);
-		// RectF r2=new RectF(9,10,100,100);
-		// Pnt.setColor(Color.RED);
-		// canvas.drawRoundRect(r,10,10,Pnt);
-		// r.set(110,10,150,100);
-		// canvas.drawOval(r,Pnt);
-//		Pnt.setColor(Color.MAGENTA);
-//		r.set(10, 110, 100, 200);
-//		canvas.drawArc(r, 0, 150, true, Pnt);
-//		Pnt.setColor(Color.GREEN);
-//		r2.set(110, 110, 200, 200);
-//		canvas.drawArc(r, 10, 150, false, Pnt);
+		Pnt.setColor(Color.WHITE);
+		canvas.drawPaint(Pnt);
+
 		Pnt.setColor(Color.YELLOW);
 		RectF rf = new RectF(190, 350, 400, 400);
-		rf.set(50,50,500,500);
+		rf.set(50, 50, 500, 500);
 		canvas.drawArc(rf, 0, imgX, true, Pnt);
-		canvas.drawText("Arc", 200, 470, Pnt);
-		
+		Pnt.setColor(Color.MAGENTA);
+		canvas.drawText("Arc", 190, 400, Pnt);
+
 		Pnt.setColor(Color.WHITE);
 		RectF rf2 = new RectF(100, 260, 200, 200);
-		rf2.set(150,150,400,400);
+		rf2.set(150, 150, 400, 400);
 		canvas.drawArc(rf2, 0, imgX, true, Pnt);
 		canvas.drawText("Arc", 200, 470, Pnt);
-		
-		
-		
-		
-		
+
+		// RectF rf3 = new RectF();
+		// Pnt.setColor(Color.BLUE);
+		// rf3.set(50, 50, 500, 500);
+		// canvas.drawArc(rf, 100, imgX, true, Pnt);
+		// canvas.drawText("Arc", 200, 470, Pnt);
+		//
+		// RectF rf4 = new RectF();
+		// Pnt.setColor(Color.WHITE);
+		// rf4.set(150, 150, 400, 400);
+		// canvas.drawArc(rf2, 0, imgX, true, Pnt);
+		// canvas.drawText("자외선", 200, 470, Pnt);
+
 		// Pnt.setColor(Color.BLUE);
 		// float[] pts={10,210,50,250,50,250,110,220};
 		// canvas.drawLines(pts,Pnt);
@@ -153,12 +172,9 @@ class MyViewThread extends Thread {
 		_canvas.drawARGB(255, 255, 255, 255);
 	}
 
-//	private void drawMyBitmap(Canvas _canvas) {
-//		_canvas.drawBitmap(testImage01, imgX, imgY, null);
-//		imgX++;
-//		imgY++;
-//	}
+	// private void drawMyBitmap(Canvas _canvas) {
+	// _canvas.drawBitmap(testImage01, imgX, imgY, null);
+	// imgX++;
+	// imgY++;
+	// }
 }
-	
-	
-

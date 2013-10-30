bc9ff53477d60daa7d2ea4baccbff4270da666a4
jungho.song@kodeglam.com (threeword) <jungho.song@kodeglam.com (threeword)@192.168.0.142>
2013-10-30 13:51:40 +0900

    NoteEdit 액션바를 이용하도록 수정

--- Collage_GoddessOfSkin-prototype/src/com/collage/goddessofskin/prototype/fragment/settings/FragSettingsProfile.java
+++ Collage_GoddessOfSkin-prototype/src/com/collage/goddessofskin/prototype/fragment/settings/FragSettingsProfile.java
@@ -2,10 +2,10 @@ package com.collage.goddessofskin.prototype.fragment.settings;
 
 import java.io.File;
 
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.ListFragment;
 import android.app.AlertDialog.Builder;
+import android.app.ListFragment;
 import android.content.ContentUris;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -24,7 +24,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.SubMenu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -38,15 +37,18 @@ import android.widget.TextView;
 import com.collage.goddessofskin.prototype.R;
 import com.collage.goddessofskin.prototype.utils.io.FileUtil;
 
-public class FragSettingsProfile extends ListFragment implements
-		OnClickListener {
+public class FragSettingsProfile extends ListFragment implements OnClickListener
+{
 	private static final int PICK_FROM_CAMERA = 0;
 	private static final int PICK_FROM_ALBUM = 1;
 	private static final int CROP_FROM_CAMERA = 2;
+
 	private static final int INSERT_ID = Menu.FIRST;
 	private static final int DELETE_ID = Menu.FIRST + 1;
-	private static final int ACTIVITY_CREATE = 3;
-	private static final int ACTIVITY_EDIT = 1;
+
+	private static final int NOTE_CREATE = 3;
+	private static final int NOTE_EDIT = NOTE_CREATE + 1;
+
 	private Uri mImageCaptureUri;
 	private ImageView mPhotoImageView;
 	private Button mbutton;
@@ -55,7 +57,8 @@ public class FragSettingsProfile extends ListFragment implements
 	private ImageView profile_image12;
 	private Button popup_Button1;
 	private Button Alarmsound_Button;
-	DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+	DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener()
+	{
 
 		@Override
 		public void onClick(DialogInterface dialog, int which)
@@ -65,39 +68,39 @@ public class FragSettingsProfile extends ListFragment implements
 		}
 	};
 
-	DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+	DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
+	{
 		@Override
-		public void onClick(DialogInterface dialog, int which) {
+		public void onClick(DialogInterface dialog, int which)
+		{
 			doTakeAlbumAction();
 		}
 	};
 
-	DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+	DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
+	{
 		@Override
-		public void onClick(DialogInterface dialog, int which) {
+		public void onClick(DialogInterface dialog, int which)
+		{
 			dialog.dismiss();
 		}
 	};
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.frag_settings_profile,
-				container, false);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View rootView = inflater.inflate(R.layout.frag_settings_profile, container, false);
 
-		mPhotoImageView = (ImageView) rootView
-				.findViewById(R.id.profile_image12);
+		mPhotoImageView = (ImageView) rootView.findViewById(R.id.profile_image12);
 		mPhotoImageView.setOnClickListener(this);
-
 
 		popup_Button1 = (Button) rootView.findViewById(R.id.popup_Button1);
 		popup_Button1.setOnClickListener(this);
-		
 
 		Alarmsound_Button = (Button) rootView.findViewById(R.id.Alarmsound_Button);
 		Alarmsound_Button.setOnClickListener(this);
 		setHasOptionsMenu(true);
-		
+
 		View view = rootView.findViewById(android.R.id.list);
 		this.registerForContextMenu(view);
 		return rootView;
@@ -106,18 +109,16 @@ public class FragSettingsProfile extends ListFragment implements
 	/**
 	 * 카메라에서 이미지 가져오기
 	 */
-	private void doTakePhotoAction() {
+	private void doTakePhotoAction()
+	{
 
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
 		// 임시로 사용할 파일의 경로를 생성
-		String url = "tmp_" + String.valueOf(System.currentTimeMillis())
-				+ ".jpg";
-		mImageCaptureUri = Uri.fromFile(new File(Environment
-				.getExternalStorageDirectory(), url));
+		String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+		mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
 		Log.d("test", mImageCaptureUri.toString());
-		intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
-				mImageCaptureUri);
+		intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
 		intent.putExtra("return-data", true);
 		startActivityForResult(intent, PICK_FROM_CAMERA);
 	}
@@ -125,7 +126,8 @@ public class FragSettingsProfile extends ListFragment implements
 	/**
 	 * 앨범에서 이미지 가져오기
 	 */
-	private void doTakeAlbumAction() {
+	private void doTakeAlbumAction()
+	{
 
 		// 앨범 호출
 		Intent intent = new Intent(Intent.ACTION_PICK);
@@ -134,131 +136,131 @@ public class FragSettingsProfile extends ListFragment implements
 	}
 
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (resultCode != Activity.RESULT_OK) return;
 
-		if (resultCode != getActivity().RESULT_OK) {
+		switch (requestCode)
+		{
+			case CROP_FROM_CAMERA:
+			{
+				// 크롭이 된 이후의 이미지를 넘겨 받습니다. 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+				// 임시 파일을 삭제합니다.
+				final Bundle extras = data.getExtras();
 
-			return;
+				if (extras != null)
+				{
+					Bitmap photo = extras.getParcelable("data");
+					mPhotoImageView.setImageBitmap(photo);
 
-		}
+					FileUtil.save(photo, getActivity().getExternalCacheDir().getAbsolutePath(), "frofile.jpg");
 
-		switch (requestCode) {
-		case CROP_FROM_CAMERA: {
-			// 크롭이 된 이후의 이미지를 넘겨 받습니다. 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-			// 임시 파일을 삭제합니다.
-			final Bundle extras = data.getExtras();
+				}
 
-			if (extras != null) {
-				Bitmap photo = extras.getParcelable("data");
-				mPhotoImageView.setImageBitmap(photo);
-
-				FileUtil.save(photo, getActivity().getExternalCacheDir()
-						.getAbsolutePath(), "frofile.jpg");
+				// 임시 파일 삭제
+				File f = new File(mImageCaptureUri.getPath());
+				if (f.exists())
+				{
+					f.delete();
+				}
 
 			}
+			break;
 
-			// 임시 파일 삭제
-			File f = new File(mImageCaptureUri.getPath());
-			if (f.exists()) {
-				f.delete();
+			case PICK_FROM_ALBUM:
+			{
+				// 이후의 처리가 카메라와 같으므로 일단 break없이 진행합니다.
+				// 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+
+				mImageCaptureUri = data.getData();
 			}
+			break;
 
+			case PICK_FROM_CAMERA:
+			{
+				// 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+				// 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+
+				Intent intent = new Intent("com.android.camera.action.CROP");
+				intent.setDataAndType(mImageCaptureUri, "image/*");
+
+				intent.putExtra("outputX", 100);
+				intent.putExtra("outputY", 100);
+				intent.putExtra("aspectX", 1);
+				intent.putExtra("aspectY", 1);
+				intent.putExtra("scale", true);
+				intent.putExtra("return-data", true);
+				startActivityForResult(intent, CROP_FROM_CAMERA);
+
+			}
+			break;
+
+			case NOTE_CREATE:
+			case NOTE_EDIT:
+			{
+				fillData();
+			}
 			break;
 		}
-
-		case PICK_FROM_ALBUM: {
-			// 이후의 처리가 카메라와 같으므로 일단 break없이 진행합니다.
-			// 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-
-			mImageCaptureUri = data.getData();
-		}
-
-		case PICK_FROM_CAMERA: {
-			// 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-			// 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-
-			Intent intent = new Intent("com.android.camera.action.CROP");
-			intent.setDataAndType(mImageCaptureUri, "image/*");
-
-			intent.putExtra("outputX", 100);
-			intent.putExtra("outputY", 100);
-			intent.putExtra("aspectX", 1);
-			intent.putExtra("aspectY", 1);
-			intent.putExtra("scale", true);
-			intent.putExtra("return-data", true);
-			startActivityForResult(intent, CROP_FROM_CAMERA);
-
-			break;
-		}
-		case ACTIVITY_CREATE: {
-			fillData();
-			break;
-		}
-
-		}
-
 	}
 
-	public void onClick(View v) {
-		
-		switch (v.getId()) {
-		case R.id.profile_image12:
-			new AlertDialog.Builder(getActivity()).setIcon(R.drawable.ic_launcher)
-		.setTitle("업로드할 이미지 선택")
-		.setPositiveButton("사진촬영", cameraListener)
-		.setNeutralButton("앨범선택", albumListener)
-		.setNegativeButton("취소", cancelListener).show();
+	public void onClick(View v)
+	{
+
+		switch (v.getId())
+		{
+			case R.id.profile_image12:
+				new AlertDialog.Builder(getActivity()).setIcon(R.drawable.ic_launcher).setTitle("업로드할 이미지 선택").setPositiveButton("사진촬영", cameraListener).setNeutralButton("앨범선택", albumListener).setNegativeButton("취소", cancelListener).show();
 			break;
-		
-			
-		
-		
-			
-		case R.id.Alarmsound_Button:
-		
-			 Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-			 intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE,"알림음 설정");
-			 Uri uri = ContentUris.withAppendedId(
-			 MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, 1l);
-			 intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
-			 intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, uri);
-			 startActivityForResult(intent, 123);
-			 
-			 break;
-		
-		case R.id.popup_Button1:
-			
-			Builder d = new AlertDialog.Builder(getActivity());
-			d.setTitle("팝업설정");
-			d.setSingleChoiceItems(R.array.select_popup, 0,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog,
-								int whichButton) {
 
-						}
+			case R.id.Alarmsound_Button:
 
-					});
-			d.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int whichButton) {
-					// 이구간은 확인버튼을 선택했을때 설정되어야하는 구간이니까 멘토님한테 물어본다.
-				}
-			});
-			d.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.cancel();
-				}
-			});
-			d.show();
-		
+				Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "알림음 설정");
+				Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, 1l);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
+				intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, uri);
+				startActivityForResult(intent, 123);
+
+			break;
+
+			case R.id.popup_Button1:
+
+				Builder d = new AlertDialog.Builder(getActivity());
+				d.setTitle("팝업설정");
+				d.setSingleChoiceItems(R.array.select_popup, 0, new DialogInterface.OnClickListener()
+				{
+					public void onClick(DialogInterface dialog, int whichButton)
+					{
+
+					}
+
+				});
+				d.setPositiveButton("확인", new DialogInterface.OnClickListener()
+				{
+					public void onClick(DialogInterface dialog, int whichButton)
+					{
+						// 이구간은 확인버튼을 선택했을때 설정되어야하는 구간이니까 멘토님한테 물어본다.
+					}
+				});
+				d.setNegativeButton("취소", new DialogInterface.OnClickListener()
+				{
+					public void onClick(DialogInterface dialog, int which)
+					{
+						dialog.cancel();
+					}
+				});
+				d.show();
+
 		}
-		
 
 	}
 
 	/**
 	 * 메모장
 	 */
-	public void onResume() {
+	public void onResume()
+	{
 		super.onResume();
 
 		dbAdapter = new NotesDbAdapter(getActivity());
@@ -268,18 +270,24 @@ public class FragSettingsProfile extends ListFragment implements
 		fillData();
 	}
 
-	private void fillData() {
+	private void fillData()
+	{
 
 		// 모든 데이터의 커서를 얻어옴
 		Cursor c = dbAdapter.fetchAllNotes();
 
 		// 리스트뷰에 데이터베이스의 저장된 데이터를 연결
-		String[] from = new String[] { BaseColumns._ID, NotesDbAdapter.TITLE };
+		String[] from = new String[]
+		{
+				BaseColumns._ID, NotesDbAdapter.TITLE
+		};
 		// 한 행을 보여줄 XML 파일의 텍스트 뷰 id
-		int to[] = { android.R.id.text1, android.R.id.text2 };
+		int to[] =
+		{
+				android.R.id.text1, android.R.id.text2
+		};
 
-		adapter = new SimpleCursorAdapter(getActivity(),
-				android.R.layout.simple_list_item_2, c, from, to);
+		adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_2, c, from, to);
 
 		setListAdapter(adapter);
 		// Activity의 라이프사이클에 따라 알아서 커서를 관리해 줌, 커서를 쓰는
@@ -289,80 +297,65 @@ public class FragSettingsProfile extends ListFragment implements
 	}
 
 	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-
-		/*
-		menu.add(Menu.NONE, INSERT_ID, Menu.NONE, "추가")
-				.setIcon(android.R.drawable.ic_menu_add)
-				.setAlphabeticShortcut('a');
-		super.onCreateOptionsMenu(menu, inflater);
-		*/
-		
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+	{
 		inflater.inflate(R.menu.frag_settings, menu);
 		super.onCreateOptionsMenu(menu, inflater);
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) 
+	public boolean onOptionsItemSelected(MenuItem item)
 	{
-		switch (item.getItemId()) 
+		switch (item.getItemId())
 		{
-			case R.id.action_new_note :
+			case R.id.action_new_note:
 			{
 				Intent intent = new Intent(getActivity(), NoteEdit.class);
-				startActivityForResult(intent, ACTIVITY_CREATE);
+				startActivityForResult(intent, NOTE_CREATE);
 			}
-			return true;
-	
-			default : return (super.onOptionsItemSelected(item));
-		}
-		
-		/*
-		switch (item.getItemId()) {
-		case INSERT_ID:
-			// NoteEdit로 아이디 없이 넘어가면 추가
-			Intent intent = new Intent(getActivity(), NoteEdit.class);
-			startActivityForResult(intent, ACTIVITY_CREATE);
+				return true;
 
-			return true;
+			default:
+				return (super.onOptionsItemSelected(item));
 		}
-		return (super.onOptionsItemSelected(item));
-		*/
 	}
 
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+	{
 		super.onCreateContextMenu(menu, v, menuInfo);
 		// 삭제 메뉴 추가
 		menu.add(Menu.NONE, DELETE_ID, Menu.NONE, "삭제");
 	}
 
 	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case DELETE_ID:
-			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-					.getMenuInfo();
-			dbAdapter.deleteNote(info.id);
-			fillData();
-			return true;
+	public boolean onContextItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case DELETE_ID:
+				AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+				dbAdapter.deleteNote(info.id);
+				fillData();
+				return true;
 		}
 		return super.onContextItemSelected(item);
 	}
 
 	// NoteEdit로 아이디 값만 넘겨줌 아이디 있으면 수정
 
-	public void onListItemClick(ListView l, View v, int position, long id) {
+	public void onListItemClick(ListView l, View v, int position, long id)
+	{
 		long rowId = -1;
 		super.onListItemClick(l, v, position, id);
 
 		Intent intent = new Intent(getActivity(), NoteEdit.class);
 		TextView tv = (TextView) v.findViewById(android.R.id.text1);
 		rowId = Long.parseLong(tv.getText().toString());
-		if (rowId != -1) {
+		if (rowId != -1)
+		{
 			intent.putExtra(NotesDbAdapter._ID, rowId);
-			startActivityForResult(intent, ACTIVITY_EDIT);
+			startActivityForResult(intent, NOTE_EDIT);
 		}
 	}
 
